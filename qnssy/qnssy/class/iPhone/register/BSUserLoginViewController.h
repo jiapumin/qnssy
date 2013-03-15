47c003aefb5f8aafcf1c76300017f7926fe852b7
cheneyjuu <cheneyjuu@163.com>
2013-03-15 20:16:25 +0800

    登录界面

---
+++ qnssy/qnssy/class/iPhone/register/BSUserLoginViewController.h
@@ -0,0 +1,26 @@
+//
+//  BSUserLoginViewController.h
+//  qnssy
+//
+//  Created by juchen on 13-3-15.
+//  Copyright (c) 2013年 jpm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSUserLoginViewController : UIViewController
+
+@property (retain, nonatomic) IBOutlet UIImageView *userLogoImage_1;
+@property (retain, nonatomic) IBOutlet UIImageView *userLogoImage_2;
+@property (retain, nonatomic) IBOutlet UIImageView *userLogoImage_3;
+@property (retain, nonatomic) IBOutlet UIImageView *userLogoImage_4;
+@property (retain, nonatomic) IBOutlet UIImageView *userLogoImage_5;
+@property (retain, nonatomic) IBOutlet UIImageView *userLogoImage_6;
+@property (retain, nonatomic) IBOutlet UIImageView *userLogoImage_7;
+@property (retain, nonatomic) IBOutlet UIImageView *userLogoImage_8;
+@property (retain, nonatomic) IBOutlet UIImageView *userLogoImage_9;
+@property (retain, nonatomic) IBOutlet UIImageView *userLogoImage_10;
+
+@property (retain, nonatomic) IBOutlet UIView *loginBackgroundView;
+
+@end
