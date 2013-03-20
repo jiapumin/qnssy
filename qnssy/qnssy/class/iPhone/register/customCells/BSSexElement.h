338fe2810a94d00e9532e878e136ca0f59f481f7
cheneyjuu <cheneyjuu@163.com>
2013-03-20 23:25:38 +0800

    注册详细信息

---
+++ qnssy/qnssy/class/iPhone/register/customCells/BSSexElement.h
@@ -0,0 +1,13 @@
+//
+//  BSSexElement.h
+//  qnssy
+//
+//  Created by juchen on 13-3-20.
+//  Copyright (c) 2013年 jpm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSSexElement : QSegmentedElement
+
+@end
