80c9fc9a72481d60e1c32119bec9b61308e698b8
kingbird <jiapumin@163.com>
2013-03-20 23:38:26 +0800

    解决冲突

---
+++ qnssy/qnssy/class/iPhone/register/customCells/BSSexElement.m
@@ -0,0 +1,61 @@
+//
+//  BSSexElement.m
+//  qnssy
+//
+//  Created by juchen on 13-3-20.
+//  Copyright (c) 2013年 jpm. All rights reserved.
+//
+
+#import "BSSexElement.h"
+
+@implementation BSSexElement{
+    QuickDialogController *_controller;
+}
+
+- (UITableViewCell *)getCellForTableView:(QuickDialogTableView *)tableView controller:(QuickDialogController *)controller {
+    _controller = controller;
+    QTableViewCell *cell = [[QTableViewCell alloc] init];
+    cell.backgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
+    cell.backgroundColor = [UIColor clearColor];
+    
+    NSMutableArray * item = [NSMutableArray arrayWithArray:_items];
+//    [item addObject:@""];
+//    [item insertObject:@"" atIndex:0];
+    CGRect frame = cell.contentView.bounds;
+    frame.origin.x -=1;
+    frame.size.width += 2;
+    frame.size.height += 1;
+    UIView * container = [[UIView alloc] initWithFrame: frame];
+    container.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    container.clipsToBounds = YES;
+    container.backgroundColor = [UIColor whiteColor];
+    [cell.contentView addSubview:container];
+    CALayer *layer = container.layer;
+    layer.cornerRadius = 8.0;
+    UIColor *color = [UIColor colorWithRed:0.603 green:0.603 blue:0.603 alpha:1];
+    CGColorRef colorref = [color CGColor];
+    layer.borderColor = colorref;
+    layer.borderWidth = 1.0;
+    
+    UILabel *title = [[UILabel alloc] init];
+    title.frame = CGRectMake(9, 0, 80, frame.size.height);
+    title.text = @"性别";
+    title.backgroundColor = [UIColor clearColor];
+    
+    UISegmentedControl *control = [[UISegmentedControl alloc] initWithItems:item];
+    [control addTarget:self action:@selector(handleSegmentedControlValueChanged:) forControlEvents:UIControlEventValueChanged];
+    frame.origin.x = frame.size.width - 100 - 10;
+    frame.size.width = 100;
+    frame.origin.y = 5;
+    frame.size.height -= 10;
+    control.frame = frame;
+    control.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    control.segmentedControlStyle = UISegmentedControlStylePlain;
+    control.selectedSegmentIndex = _selected;
+    control.tag = 4321;
+    [container addSubview:title];
+    [container addSubview:control];
+    return cell;
+}
+
+@end
--- qnssy/qnssy/class/iPhone/register/customCells/BSUserSexCell.m
+++ qnssy/qnssy/class/iPhone/register/customCells/BSUserSexCell.m
@@ -10,6 +10,39 @@
 
 @implementation BSUserSexCell
 
+- (id) init {
+    self = [super init];
+    if (self) {
+        UIView *backgroundView = [[UIView alloc] initWithFrame:CGRectMake(10, 0, 280, 44)];
+        [backgroundView setBackgroundColor:[UIColor whiteColor]];
+        
+        CALayer *layer = backgroundView.layer;
+        layer.cornerRadius = 8.0;
+        UIColor *color = [UIColor colorWithRed:0.603 green:0.603 blue:0.603 alpha:1];
+        CGColorRef colorref = [color CGColor];
+        layer.borderColor = colorref;
+        layer.borderWidth = 1.0;
+        
+        [self setBackgroundView:backgroundView];
+        
+        UILabel *sexLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, 50, 44)];
+        sexLabel.backgroundColor = [UIColor clearColor];
+        sexLabel.text = @"性别";
+        UIColor *labelColor = [UIColor colorWithRed:0.333 green:0.333 blue:0.333 alpha:1];
+        sexLabel.textColor = labelColor;
+        
+        NSArray *segmentedArray = [[NSArray alloc] initWithObjects:@"男",@"女", nil];
+        UISegmentedControl *segmentedControl = [[UISegmentedControl alloc] initWithItems:segmentedArray];
+        segmentedControl.selectedSegmentIndex = 0;
+        segmentedControl.frame = CGRectMake(backgroundView.frame.size.width - 70, 5, 80, 34);
+        
+        [self.contentView addSubview:sexLabel];
+        [self.contentView addSubview:segmentedControl];
+        [sexLabel release];
+    }
+    return self;
+}
+
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
