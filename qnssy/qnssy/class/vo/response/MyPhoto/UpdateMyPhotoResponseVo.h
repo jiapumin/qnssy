20d577048c2ce20e3528ebd5f2dd8077f9922f09
kingbird <jiapumin@163.com>
2013-05-20 16:15:09 +0800

    上传图片处理
    Signed-off-by: kingbird <jiapumin@163.com>

---
+++ qnssy/qnssy/class/vo/response/MyPhoto/UpdateMyPhotoResponseVo.h
@@ -0,0 +1,13 @@
+//
+//  UpdateMyPhotoResponseVo.h
+//  qnssy
+//
+//  Created by jpm on 13-5-20.
+//  Copyright (c) 2013年 jpm. All rights reserved.
+//
+
+#import "SuperResponseVo.h"
+
+@interface UpdateMyPhotoResponseVo : SuperResponseVo
+
+@end
---
+++ qnssy/qnssy/class/vo/response/MyPhoto/UpdateMyPhotoResponseVo.m
@@ -0,0 +1,13 @@
+//
+//  UpdateMyPhotoResponseVo.m
+//  qnssy
+//
+//  Created by jpm on 13-5-20.
+//  Copyright (c) 2013年 jpm. All rights reserved.
+//
+
+#import "UpdateMyPhotoResponseVo.h"
+
+@implementation UpdateMyPhotoResponseVo
+
+@end
